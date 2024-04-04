5f7fbaad6769c7b4da84d88235dd1d7de3490048
CristianUrsino <ursino.cristian.uc@gmail.com>
2024-04-04 15:06:16 +0200

    added Eagle class

--- src/org/learning/Eagle.java
+++ src/org/learning/Eagle.java
@@ -1,4 +1,17 @@
 package org.learning;
 
-public class Eagle {
+public class Eagle extends Animal{
+    //costruttore
+    public Eagle(String name){
+        super(name);
+    }
+    //metodi
+    @Override
+    public void eat() {
+        System.out.println(getName() + " eat meat");
+    }
+    @Override
+    public void makeNoise() {
+        System.out.println("AhAAaa Ahaa Ahaa!");
+    }
 }
