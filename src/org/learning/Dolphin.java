cf0982b5c8e085b8e8e69c10d4ecddf124908aa3
CristianUrsino <ursino.cristian.uc@gmail.com>
2024-04-04 15:14:40 +0200

    added Dolphin class

--- src/org/learning/Dolphin.java
+++ src/org/learning/Dolphin.java
@@ -1,4 +1,17 @@
 package org.learning;
 
-public class Dolphin {
+public class Dolphin extends Animal{
+    //costruttore
+    public Dolphin(String name){
+        super(name);
+    }
+    //metodi
+    @Override
+    public void eat() {
+        System.out.println(getName() + " eat small fish");
+    }
+    @Override
+    public void makeNoise() {
+        System.out.println("Ih IIiih");
+    }
 }
