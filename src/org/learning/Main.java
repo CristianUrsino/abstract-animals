0eedc1355ac4a794e882981638da8d3dde53ed5a
CristianUrsino <ursino.cristian.uc@gmail.com>
2024-04-04 15:28:05 +0200

    added Main

--- src/org/learning/Main.java
+++ src/org/learning/Main.java
@@ -1,7 +1,62 @@
 package org.learning;
 
 public class Main {
-    public static void main(String[] args) {
 
+    private static Dolphin dolphin;
+
+    public static void main(String[] args) {
+        //Inizializzo un Dog
+        Dog dog = null;
+        try {
+            dog = new Dog("Fuffy");
+        } catch (IllegalArgumentException e) {
+            System.out.println(e);
+        }
+        //richiamo tutti i metodi
+        System.out.println("Dog: " + dog.getName());
+        dog.swim();
+        dog.eat();
+        dog.makeNoise();
+        dog.sleap();
+        //inizializzo uno Sparrow
+        Sparrow sparrow = null;
+        try {
+            sparrow = new Sparrow("Paperino");
+        } catch (IllegalArgumentException e) {
+            System.out.println(e);
+        }
+        //richiamo tutti i metodi
+        System.out.println("\nSparrow: " + sparrow.getName());
+        sparrow.fly();
+        sparrow.eat();
+        sparrow.swim();
+        sparrow.makeNoise();
+        sparrow.sleap();
+        //inizializzo uno Eagle
+        Eagle eagle = null;
+        try {
+            eagle = new Eagle("Pippo");
+        } catch (IllegalArgumentException e) {
+            System.out.println(e);
+        }
+        //richiamo tutti i metodi
+        System.out.println("\nEagle: " + sparrow.getName());
+        eagle.fly();
+        eagle.eat();
+        eagle.makeNoise();
+        eagle.sleap();
+        //inizializzo un Dolphin
+        dolphin = null;
+        try {
+            dolphin = new Dolphin("pluto");
+        } catch (IllegalArgumentException e) {
+            System.out.println(e);
+        }
+        //richiamo tutti i metodi
+        System.out.println("\nDolphin: " + dolphin.getName());
+        dolphin.swim();
+        dolphin.eat();
+        dolphin.makeNoise();
+        dolphin.sleap();
     }
 }
