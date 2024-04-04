52a4cd772bb73bbe54bff7890dcbcdd95f28435c
CristianUrsino <ursino.cristian.uc@gmail.com>
2024-04-04 14:49:51 +0200

    updated Animal with name and validation

--- src/org/learning/Animal.java
+++ src/org/learning/Animal.java
@@ -1,9 +1,28 @@
 package org.learning;
 
 public abstract class Animal {
+    //attributi
+    private String name;
+    //costruttori
+    public Animal(String name)
+            throws IllegalArgumentException{
+        if(name.isEmpty()) throw new IllegalArgumentException("The field name is empty");
+        this.name = name;
+    }
+    //setter
+    public void setName(String name)
+            throws IllegalArgumentException{
+        if(name.isEmpty()) throw new IllegalArgumentException("The field name is empty");
+        this.name = name;
+    }
+    //getter
+    public String getName() {
+        return name;
+    }
+    //metodi
     public void sleap(){
         System.out.println("ZZZzzz");
     }
     public abstract void makeNoise();
-    public abstract void Eat();
+    public abstract void eat();
 }
