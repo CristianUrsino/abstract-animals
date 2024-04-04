9d760cfaedbf41f633fe89a0506aa473314bc1f1
CristianUrsino <ursino.cristian.uc@gmail.com>
2024-04-04 14:17:37 +0200

    added Animal abstract class

--- src/org/learning/Animal.java
+++ src/org/learning/Animal.java
@@ -1,4 +1,9 @@
 package org.learning;
 
-public class Animal {
+public abstract class Animal {
+    public void sleap(){
+        System.out.println("ZZZzzz");
+    }
+    public abstract void makeNoise();
+    public abstract void Eat();
 }
