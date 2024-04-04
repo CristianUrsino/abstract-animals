73f33af583f8cad0be774e48a7ccf8da8ad1d617
CristianUrsino <ursino.cristian.uc@gmail.com>
2024-04-04 14:50:08 +0200

    added Dog class

--- src/org/learning/Dog.java
+++ src/org/learning/Dog.java
@@ -1,4 +1,17 @@
 package org.learning;
 
-public class Dog {
+public class Dog extends Animal{
+    //costruttore
+    public Dog(String name){
+        super(name);
+    }
+    //methods
+    @Override
+    public void eat() {
+        System.out.println(getName() + " eat meat");
+    }
+    @Override
+    public void makeNoise() {
+        System.out.println("Bau!");
+    }
 }
