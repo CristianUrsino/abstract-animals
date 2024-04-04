321355b0b1499fb08580cb6edb1805fee43db9cf
CristianUrsino <ursino.cristian.uc@gmail.com>
2024-04-04 14:59:19 +0200

    added Sparrow class

--- src/org/learning/Sparrow.java
+++ src/org/learning/Sparrow.java
@@ -1,4 +1,17 @@
 package org.learning;
 
-public class Sparrow {
+public class Sparrow extends Animal{
+    //costruttore
+    public Sparrow(String name){
+        super(name);
+    }
+    //metodi
+    @Override
+    public void eat() {
+        System.out.println(getName() + " eat seeds");
+    }
+    @Override
+    public void makeNoise() {
+        System.out.println("Qua qua!");
+    }
 }
